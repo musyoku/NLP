f5f631a0443087592795ed5dd1f7c43d911bce12
musyoku <musyoku@users.noreply.github.com>
2016-04-10 00:22:16 +0900

    j

--- beluga_lstm/vocab.py
+++ beluga_lstm/vocab.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
+import os
 import numpy as np
 
 vocab = {}
 inv_vocab = {}
 
 def load_data(dir):
-	words = open(filename).read().replace('\n', '<eos>').strip().split()
-	dataset = np.ndarray((len(words),), dtype=np.int32)
-	for i, word in enumerate(words):
-		if word not in vocab:
-			vocab[word] = len(vocab)
-		dataset[i] = vocab[word]
+	fs = os.listdir(dir)
+	print "loading", len(fs), "files..."
+	dataset = None
+	for fn in fs:
+		words = open("%s/%s" % (dir, fn)).read().replace("\n", "<eos>").strip().split()
+		data = np.empty((len(words),), dtype=np.int32)
+		for i, word in enumerate(words):
+			if word not in vocab:
+				vocab[word] = len(vocab)
+			data[i] = vocab[word]
+		if dataset is None:
+			dataset = data
+		else:
+			dataset = np.append(dataset, data)
 	return dataset
