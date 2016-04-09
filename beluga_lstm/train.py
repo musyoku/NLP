e0a62af8d4ec1dd20c217b606beb2d33ad7a6d4e
musyoku <musyoku@users.noreply.github.com>
2016-04-10 00:37:36 +0900

    j

--- beluga_lstm/train.py
+++ beluga_lstm/train.py
@@ -2,5 +2,8 @@
 import os, sys, time
 import numpy as np
 import model
+import vocab
 
+data_dir = "text"
+dataset = vocab.load(data_dir)
 beluga = model.build()
--- beluga_lstm/vocab.py
+++ beluga_lstm/vocab.py
@@ -1,23 +1,25 @@
 # -*- coding: utf-8 -*-
 import os
 import numpy as np
+import codecs
 
 vocab = {}
 inv_vocab = {}
 
-def load_data(dir):
+def load(dir):
 	fs = os.listdir(dir)
 	print "loading", len(fs), "files..."
-	dataset = None
+	dataset = []
 	for fn in fs:
-		words = open("%s/%s" % (dir, fn)).read().replace("\n", "<eos>").strip().split()
-		data = np.empty((len(words),), dtype=np.int32)
-		for i, word in enumerate(words):
-			if word not in vocab:
-				vocab[word] = len(vocab)
-			data[i] = vocab[word]
-		if dataset is None:
-			dataset = data
-		else:
-			dataset = np.append(dataset, data)
+		unko = codecs.open("%s/%s" % (dir, fn), "r", "utf_8_sig")
+		for line in unko:
+			data = np.empty((len(line),), dtype=np.int32)
+			for i in xrange(len(line)):
+				word = line[i]
+				if word not in vocab:
+					vocab[word] = len(vocab)
+				data[i] = vocab[word]
+			dataset.append(data)
+	print "# of chars:", len(vocab)
+	print "# of data:", len(dataset)
 	return dataset
