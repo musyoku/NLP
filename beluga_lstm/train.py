01bbcee91aad86eb559e3bb821f41f06f0661195
musyoku <musyoku@users.noreply.github.com>
2016-04-10 14:43:38 +0900

    j

--- beluga_lstm/train.py
+++ beluga_lstm/train.py
@@ -11,28 +11,37 @@ lm = model.build(n_vocab)
 lm.load(model_dir)
 
 n_epoch = 1000
-n_train = 1000
+n_train = 5000
 batchsize = 32
 total_time = 0
+max_length_of_data = 100
+length_limit = 10
+
 for epoch in xrange(n_epoch):
 	start_time = time.time()
 	sum_loss = 0
 	for t in xrange(n_train):
 		batch_array = []
-		max_length = 0
+		max_length_in_batch = 0
 		for b in xrange(batchsize):
-			k = np.random.randint(0, n_dataset)
-			data = dataset[k]
+			length = length_limit + 1
+			while length > length_limit:
+				k = np.random.randint(0, n_dataset)
+				data = dataset[k]
+				length = len(data)
 			batch_array.append(data)
-			if len(data) > max_length:
-				max_length = len(data)
-		batch = np.full((batchsize, max_length), -1.0, dtype=np.float32)
+			if length > max_length_in_batch:
+				max_length_in_batch = length
+		batch = np.full((batchsize, max_length_in_batch), -1.0, dtype=np.float32)
 		for i, data in enumerate(batch_array):
 			batch[i,:len(data)] = data
 		lm.reset_state()
 		sum_loss += lm.learn(batch)
+		if t % 100 == 0:
+		    sys.stdout.write("\r%d/%d" % (t, n_train))
+		    sys.stdout.flush()
 	elapsed_time = time.time() - start_time
 	total_time += elapsed_time
-	print "epoch:", epoch, "loss:", sum_loss / float(n_train), "time:", int(elapsed_time / 60), "min", "total_time:", int(total_time / 60), "min"
-	lm.save(model_dir)
-
+	print "epoch:", epoch, "loss:", sum_loss / float(n_train), "time:", int(elapsed_time / 60), "min", "total_time:", int(total_time / 60), "min", "length_limit:", length_limit
+	# lm.save(model_dir)
+	length_limit = (length_limit + 5) if length_limit < max_length_of_data else max_length_of_data
