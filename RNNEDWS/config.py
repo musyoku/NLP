c0824a61610c9fb7e05aab4fbb6ae947a29ac9fc
musyoku <musyoku@hellowork.jp>
2016-04-15 12:21:25 +0900

    j

---
+++ RNNEDWS/config.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import argparse
+from activations import activations
+
+class Config(object):
+	def __init__(self):
+		self.use_gpu = True
+		self.learning_rate = 0.00025
+		self.gradient_momentum = 0.95
+
+		# 文字埋め込みベクトルの次元数
+		self.embed_size = 200
+
+		# 各LSTMレイヤのユニット数を入力側から出力側に向かって並べる
+		## e.g 500(input vector)->250->100(output vector)
+		## self.q_fc_units = [500, 250, 100]
+		self.enc_lstm_units = [self.embed_size, 1024]
+		self.enc_lstm_apply_batchnorm = False
+		self.enc_lstm_apply_batchnorm_to_input = False
+		self.enc_lstm_apply_dropout = False
+
+		self.dec_lstm_apply_batchnorm = False
+		self.dec_lstm_apply_batchnorm_to_input = False
+		self.dec_lstm_apply_dropout = False
+
+		self.dec_fc_apply_batchnorm = False
+		self.dec_fc_apply_batchnorm_to_input = False
+		self.dec_fc_apply_batchnorm_to_output = False
+		self.dec_fc_apply_dropout = False
+		self.dec_fc_activation_function = "elu"
+
+	@property
+	def n_vocab(self):
+		return self._n_vocab
+
+	@n_vocab.setter
+	def n_vocab(self, value):
+		self.dec_lstm_units = [self.enc_lstm_units[-1] + value, 1024]
+		# Decoder LSTM出力をIDに変換する全結合層の各ユニット数 
+		## 出力であるソフトマックス層は自動的に挿入されます
+		self.dec_fc_units = [self.dec_lstm_units[-1], 1024]
+		self._n_vocab = value
+
+	def check(self):
+		if len(self.enc_lstm_units) < 2:
+			raise Exception("You need to add one or more hidden layers to LSTM network.")
+		if len(self.dec_lstm_units) < 2:
+			raise Exception("You need to add one or more hidden layers to LSTM network.")
+		if len(self.dec_fc_units) < 1:
+			raise Exception("You need to add one or more hidden layers to fully-connected network.")
+
+config = Config()
---
+++ RNNEDWS/eval.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import os, sys, time, codecs
+import numpy as np
+import model
+import vocab
+from config import config
+
+# Windowsでprintする用
+sys.stdout = codecs.getwriter(sys.stdout.encoding)(sys.stdout, errors="xmlcharrefreplace")
+
+data_dir = "debug"
+model_dir = "model"
+dataset, config.n_vocab, config.n_dataset = vocab.load(data_dir)
+lm = model.build()
+lm.load(model_dir)
+
+def sample_seq():
+	target_batch_array = []
+	max_length_in_batch = 0
+	k = np.random.randint(0, config.n_dataset)
+	target_seq = dataset[k]
+	source_seq = target_seq[::-1]
+	return source_seq, target_seq
+
+for phrase in xrange(10):
+	source_seq, target_seq = sample_seq()
+	lm.reset_state()
+	y_ids = lm.decode(source_seq, sampling_y=True)
+	print "source_seq", source_seq
+	print "target_seq", target_seq
+	print "decode", y_ids
+	print vocab.ids_to_str(y_ids)
