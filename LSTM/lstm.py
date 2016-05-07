6604697df0fee04e4e821aefc9b7b9ed076c318e
musyoku <musyoku@users.noreply.github.com>
2016-05-07 21:20:38 +0900

    j

--- LSTM/lstm.py
+++ LSTM/lstm.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os, time
 import numpy as np
+import chainer
 from chainer import cuda, Variable, optimizers, serializers, function, link
 from chainer.utils import type_check
 from chainer import functions as F
@@ -24,17 +25,19 @@ class Conf:
 		# 文字埋め込みベクトルの次元数
 		self.embed_size = 200
 
-		# 各LSTMレイヤのユニット数を入力側から出力側に向かって並べる
+		# 各隠れ層のユニット数を入力側から出力側に向かって並べる
+		# Unit sizes of each hidden layers
 		# e.g 500(input vector)->250->100(output vector)
-		# self.q_fc_units = [500, 250, 100]
-		self.lstm_units = [self.embed_size, 1000]
+		# q_fc_units = [250]
+		# We don't contain input and output unit size here.
+		self.lstm_hidden_units = [1000]
 		# if true, it uses BNLSTM
 		self.lstm_apply_batchnorm = False
 		self.lstm_apply_dropout = False
 
 		# Fully-connected network that converts an output of the LSTM to a label distribution or an embed vector
-		# We don't contain output unit size here.
-		self.fc_units = [self.lstm_units[-1], 500]
+		# We don't contain input and output unit size here.
+		self.fc_hidden_units = [500]
 		
 		self.fc_apply_batchnorm = False
 		self.fc_apply_dropout = False
@@ -42,30 +45,25 @@ class Conf:
 
 		# "embed_vector": outputs an embed vector. Instead of softmax layer, We use EmbedID.reverse() to convert vector to label id.
 		# "label_softmax": outputs a probability distribution of label ids using softmax layer
-		self.fc_output_type = "embed_vector"
+		self.fc_output_type = LSTM.OUTPUT_TYPE_EMBED_VECTOR
 
 		self.learning_rate = 0.0025
 		self.gradient_momentum = 0.95
 
 	def check(self):
-		if len(self.lstm_units) < 2:
+		if len(self.lstm_hidden_units) < 1:
 			raise Exception("You need to add one or more hidden layers to LSTM network.")
-		if len(self.fc_units) < 1:
+		if len(self.fc_hidden_units) < 1:
 			raise Exception("You need to add one or more hidden layers to fully-connected network.")
-		if self.fc_output_type is not in ["embed_vector", "label_softmax"]:
-			raise Exception("Invalid output type of the fully-connected layer.")
-
 
 class LSTMNetwork(chainer.Chain):
 	def __init__(self, **layers):
-		super(LSTM, self).__init__(**layers)
+		super(LSTMNetwork, self).__init__(**layers)
 		self.n_layers = 0
 		self.apply_dropout = False
 
 	def forward_one_step(self, x, test):
 		chain = [x]
-		embed = self.embed_id(chain[-1])
-		chain.append(embed)
 
 		# Hidden layers
 		for i in range(self.n_layers):
@@ -116,13 +114,7 @@ class LSTM:
 	OUTPUT_TYPE_SOFTMAX = 1
 	OUTPUT_TYPE_EMBED_VECTOR = 2
 	def __init__(self, conf, name="lstm"):
-		conf.check()
-		if conf.fc_output_type == "embed_vector":
-			self.output_type = self.OUTPUT_TYPE_EMBED_VECTOR
-		elif conf.fc_output_type == "label_softmax":
-			self.output_type = self.OUTPUT_TYPE_SOFTMAX
-		else:
-			raise Exception()
+		self.output_type = conf.fc_output_type
 		self.embed_id, self.lstm, self.fc = self.build(conf)
 		self.name = name
 		self.optimizer_lstm = optimizers.Adam(alpha=conf.learning_rate, beta1=conf.gradient_momentum)
@@ -137,14 +129,17 @@ class LSTM:
 		self.optimizer_embed_id.setup(self.embed_id)
 		self.optimizer_embed_id.add_hook(chainer.optimizer.GradientClipping(10.0))
 
-	def build(conf):
+	def build(self, conf):
 		conf.check()
 		wscale = 1.0
 
-		embed_id = L.EmbedID(conf.n_vocab, conf.lstm_units[0])
+		embed_id = L.EmbedID(conf.n_vocab, conf.embed_size)
+		if conf.use_gpu:
+			embed_id.to_gpu()
 
 		lstm_attributes = {}
-		lstm_units = zip(conf.lstm_units[:-1], conf.lstm_units[1:])
+		lstm_units = [(conf.embed_size, conf.lstm_hidden_units[0])]
+		lstm_units += zip(conf.lstm_hidden_units[:-1], conf.lstm_hidden_units[1:])
 
 		for i, (n_in, n_out) in enumerate(lstm_units):
 			if conf.lstm_apply_batchnorm:
@@ -159,11 +154,12 @@ class LSTM:
 			lstm.to_gpu()
 
 		fc_attributes = {}
-		fc_units = zip(conf.fc_units[:-1], conf.fc_units[1:])
-		if conf.fc_output_type == "embed_vector":
-			fc_units += [(conf.fc_units[-1], conf.embed_size)]
-		elif conf.fc_output_type == "label_softmax":
-			fc_units += [(conf.fc_units[-1], conf.n_vocab)]
+		fc_units = [(conf.lstm_hidden_units[-1], conf.fc_hidden_units[0])]
+		fc_units += zip(conf.fc_hidden_units[:-1], conf.fc_hidden_units[1:])
+		if conf.fc_output_type == self.OUTPUT_TYPE_EMBED_VECTOR:
+			fc_units += [(conf.fc_hidden_units[-1], conf.embed_size)]
+		elif conf.fc_output_type == self.OUTPUT_TYPE_SOFTMAX:
+			fc_units += [(conf.fc_hidden_units[-1], conf.n_vocab)]
 		else:
 			raise Exception()
 
@@ -182,7 +178,8 @@ class LSTM:
 		return embed_id, lstm, fc
 
 	def __call__(self, x, test=False, softmax=True):
-		output = self.lstm(x, test=test)
+		output = self.embed_id(x)
+		output = self.lstm(output, test=test)
 		output = self.fc(output, test=test)
 		if softmax:
 			output = F.softmax(output)
@@ -218,9 +215,17 @@ class LSTM:
 		for c0, c1 in zip(seq_batch[:-1], seq_batch[1:]):
 			c0[c0 == -1] = 0
 			c0 = Variable(xp.asanyarray(c0, dtype=np.int32))
-			c1 = Variable(xp.asanyarray(c1, dtype=np.int32))
 			output = self(c0, test=test, softmax=False)
-			loss = F.softmax_cross_entropy(output, c1)
+			if self.output_type == self.OUTPUT_TYPE_SOFTMAX:
+				c1 = Variable(xp.asanyarray(c1, dtype=np.int32))
+				loss = F.softmax_cross_entropy(output, c1)
+			elif self.output_type == self.OUTPUT_TYPE_EMBED_VECTOR:
+				c1[c1 == -1] = 0
+				c1 = Variable(xp.asanyarray(c1, dtype=np.int32))
+				target = Variable(self.embed_id(c1).data)
+				loss = F.mean_squared_error(output, target)
+			else:
+				raise Exception()
 			sum_loss += loss
 		self.optimizer_lstm.zero_grads()
 		self.optimizer_fc.zero_grads()
@@ -229,12 +234,17 @@ class LSTM:
 		self.optimizer_fc.update()
 		return sum_loss.data
 
+	def should_save(self, prop):
+		if isinstance(prop, chainer.Chain) or isinstance(prop, chainer.optimizer.GradientMethod) or isinstance(prop, L.EmbedID):
+			return True
+		return False
+
 	def load(self, dir=None):
 		if dir is None:
 			raise Exception()
 		for attr in vars(self):
 			prop = getattr(self, attr)
-			if isinstance(prop, chainer.Chain) or isinstance(prop, chainer.optimizer.GradientMethod):
+			if self.should_save(prop):
 				filename = dir + "/%s_%s.hdf5" % (self.name, attr)
 				if os.path.isfile(filename):
 					print "loading",  filename
@@ -252,6 +262,6 @@ class LSTM:
 			pass
 		for attr in vars(self):
 			prop = getattr(self, attr)
-			if isinstance(prop, chainer.Chain) or isinstance(prop, chainer.optimizer.GradientMethod):
+			if self.should_save(prop):
 				serializers.save_hdf5(dir + "/%s_%s.hdf5" % (self.name, attr), prop)
 		print "model saved."
